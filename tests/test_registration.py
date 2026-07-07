60d42428923397c128d552cc89c8ff445d417a7f
VEKul31 <Sev7715@gmail.com>
2026-07-07 19:17:45 +0300

    Практикуемся в реализации PageComponent 2.0

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,4 +1,3 @@
-from playwright.sync_api import expect, Page
 import pytest
 
 from pages.registration_page import RegistrationPage
@@ -8,29 +7,11 @@ from pages.dashboard_page import DashboardPage
 @pytest.mark.registration
 def test_successful_registration(dashboard_page: DashboardPage, registration_page: RegistrationPage):
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(
+    registration_page.registration_form.fill(
         email="user.name@gmail.com",
         username="username",
         password="password"
     )
     registration_page.click_registration_button()
 
-    dashboard_page.check_visible_dashboard_title()
-
-
-    # chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    #
-    # email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    # email_input.fill('user.name@gmail.com')
-    #
-    # username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    # username_input.fill('username')
-    #
-    # password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    # password_input.fill('password')
-    #
-    # registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    # registration_button.click()
-    #
-    # dashboard_header = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-    # expect(dashboard_header).to_be_visible()
+    dashboard_page.dashboard_toolbar.check_visible()
